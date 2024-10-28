65ead127b08310d4e205d652a5a105f8a54ccccf
candyliaa <astronomy.dev@gmail.com>
2024-10-28 19:20:19 +0200

    solved ex. 15

--- viikko1/nhl-statistics-1/src/statistics_service.py
+++ viikko1/nhl-statistics-1/src/statistics_service.py
@@ -3,8 +3,8 @@ from player_reader import PlayerReader
 
 
 class StatisticsService:
-    def __init__(self):
-        reader = PlayerReader()
+    def __init__(self, player_reader):
+        reader = player_reader
 
         self._players = reader.get_players()
 
@@ -41,3 +41,7 @@ class StatisticsService:
             i += 1
 
         return result
+
+stats = StatisticsService(
+  PlayerReader("https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt")
+)
