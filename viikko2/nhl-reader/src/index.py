f360ddf15095f0756f93ce2e62092e83c6a387dc
candyliaa <astronomy.dev@gmail.com>
2024-11-04 19:17:01 +0200

    completed week 2 ex. 12

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -2,24 +2,40 @@ import requests
 from player import Player
 
 
+class PlayerReader:
+    def __init__(self, url):
+        self._url = url
+        self.response = requests.get(self._url).json()
+
+    def get_players(self):
+        return self.response
+
+
+class PlayerStats:
+    def __init__(self, reader):
+        self.reader = reader
+        self.players = self.reader.get_players()
+
+    def top_scorers_by_nationality(self, nationality):
+        player_dicts = self.reader.get_players()
+        players = []
+        for player_dict in player_dicts:
+            if player_dict["nationality"] == nationality:
+                player = Player(player_dict)
+                players.append(player)
+        players_sorted = sorted(
+            players, key=lambda p: (p.goals + p.assists), reverse=True
+        )
+        return players_sorted
+
+
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
-    response = requests.get(url).json()
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
-    print("JSON-muotoinen vastaus:")
-    print(response)
-
-    players = []
-
-    for player_dict in response:
-        if player_dict["nationality"] == "FIN":
-            player = Player(player_dict)
-            players.append(player)
-
-    print("Players from FIN")
-
-    players_sorted = sorted(players, key=lambda p: (p.goals + p.assists), reverse=True)
-    for player in players_sorted:
+    for player in players:
         print(player)
 
 
