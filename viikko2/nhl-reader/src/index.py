c550d2e3d76a3a30d439a20a06d2e16ba89a59cd
candyliaa <astronomy.dev@gmail.com>
2024-11-04 19:48:02 +0200

    completed week 2 ex. 13

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,4 +1,6 @@
 import requests
+from rich import print
+from rich.table import Table
 from player import Player
 
 
@@ -30,13 +32,67 @@ class PlayerStats:
 
 
 def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
+    print("NHL statistics by nationality")
+    print(
+        "\nSelect season [bright_magenta][2018-19/2019-20/2020-21/2021-22/2022-23/2023-24/2024-25][/bright_magenta] "
+    )
+    season = str(input())
+    if season not in [
+        "2018-19",
+        "2019-20",
+        "2020-21",
+        "2021-22",
+        "2022-23",
+        "2023-24",
+        "2024-25",
+    ]:
+        print("\nNot a valid season! Exiting program...")
+        exit()
+    url = f"https://studies.cs.helsinki.fi/nhlstats/{season}/players"
     reader = PlayerReader(url)
     stats = PlayerStats(reader)
-    players = stats.top_scorers_by_nationality("FIN")
-
-    for player in players:
-        print(player)
+    while True:
+        print(
+            "\nSelect nationality [bright_magenta][AUT/CZE/AUS/SWE/GER/DEN/SUI/SVK/NOR/RUS/CAN/LAT/BLR/SLO/USA/FIN/GBR][/bright_magenta] "
+        )
+        nationality = str(input())
+        if nationality not in [
+            "AUT",
+            "CZE",
+            "AUS",
+            "SWE",
+            "GER",
+            "DEN",
+            "SUI",
+            "SVK",
+            "NOR",
+            "RUS",
+            "CAN",
+            "LAT",
+            "BLR",
+            "SLO",
+            "USA",
+            "FIN",
+            "GBR",
+        ]:
+            print("Not a valid nationality! Exiting program...")
+            exit()
+        table = Table(title=f"Top scorers of {nationality} season {season}")
+        table.add_column("name", style="blue")
+        table.add_column("team", style="magenta")
+        table.add_column("goals", style="green")
+        table.add_column("assists", style="green")
+        table.add_column("points", style="green")
+        players = stats.top_scorers_by_nationality(nationality)
+        for player in players:
+            table.add_row(
+                str(player.name),
+                str(player.team),
+                str(player.goals),
+                str(player.assists),
+                str((player.goals + player.assists)),
+            )
+        print(table)
 
 
 main()
