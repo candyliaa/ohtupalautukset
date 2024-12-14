c6c0559ac1f6f57a384eddf5a43d7659413a1ae3
candyliaa <astronomy.dev@gmail.com>
2024-12-14 14:21:14 +0200

    added referaatti.md

--- viikko7/src/index.py
+++ viikko7/src/index.py
@@ -15,26 +15,18 @@ def main():
         vastaus = input()
 
         if vastaus.endswith("a"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
-
             kaksinpeli = Game.create_pvp_game()
             kaksinpeli.pelaa()
+
         elif vastaus.endswith("b"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
 
             yksinpeli = Game.create_ai_game()
             yksinpeli.pelaa()
-        elif vastaus.endswith("c"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
 
+        elif vastaus.endswith("c"):
             haastava_yksinpeli = Game.create_difficult_ai_game()
             haastava_yksinpeli.pelaa()
+
         else:
             break
 
